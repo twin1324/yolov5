9b6f5dec3b37f205fa501aa4ba4d34cdae5207bd
twin1324 <126049667+twin1324@users.noreply.github.com>
2023-02-23 15:01:04 +0900

    Update split.py
    Signed-off-by: twin1324 <126049667+twin1324@users.noreply.github.com>

--- split.py
+++ split.py
@@ -1,12 +1,12 @@
 import glob
 from sklearn.model_selection import train_test_split
 
-img_list = sorted(glob.glob("content/yolov5/training/images/*.png"))
+img_list = sorted(glob.glob("/content/yolov5/training/images/*.png"))
 
 train_list, val_list = train_test_split(img_list, test_size=0.2, random_state=2000)
 
-with open("content/yolov5/train.txt", "w") as f:
+with open("/content/yolov5/train.txt", "w") as f:
     f.write("\n".join(train_list) + "\n")
 
-with open("content/yolov5/val.txt", "w") as f:
+with open("/content/yolov5/val.txt", "w") as f:
     f.write("\n".join(val_list) + "\n")
